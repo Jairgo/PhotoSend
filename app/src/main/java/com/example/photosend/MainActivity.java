bd7756b0b6ce6da0f7e96f4343c772cf0ac0c14f
Jair Gómez Vásquez <36084661+Jairgo@users.noreply.github.com>
2021-11-13 17:22:26 -0600

    Redactar y enviar mensaje. * Ya se puede escribir el mensaje que se desea mandar mediante correo, y enviar el correo elctroni

--- app/src/main/java/com/example/photosend/MainActivity.java
+++ app/src/main/java/com/example/photosend/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.photosend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.photosend.databinding.ActivityMainBinding;
 import com.example.photosend.databinding.ActivityStage1Binding;
@@ -13,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private View view;
     private Intent intent;
+    private String emailSent;
+    private Toast message;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         view = binding.getRoot();
         setContentView(view);
+        intent = getIntent();
+
+        if(intent != null){
+            emailSent = intent.getStringExtra("emailSent");
+            if(emailSent != null){
+                message = Toast.makeText(this,"The email was sent successfully", Toast.LENGTH_LONG);
+                message.getView().setBackgroundColor(Color.rgb(215, 234, 217));
+
+                if(emailSent.equals("true"))
+                    message.show();
+            }
+        }
+
+
 
         binding.btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/photosend/Stage3Activity.java
+++ app/src/main/java/com/example/photosend/Stage3Activity.java
@@ -1,12 +1,20 @@
 package com.example.photosend;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.photosend.databinding.ActivityStage1Binding;
@@ -19,9 +27,22 @@ public class Stage3Activity extends AppCompatActivity {
     private ActivityStage3Binding binding;
     private View view;
     private Intent intent;
-    private String uriString, name, email;
+    private String uriString, name, email, message;
     private InputStream Stream;
     private Uri uri;
+
+    private final ActivityResultLauncher resultMailLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == Activity.RESULT_OK){
+                binding.btnSendMail.setText("Email sent");
+                Intent endIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+                endIntent.putExtra("emailSent", "true");
+                startActivity(endIntent);
+            }
+        }
+    });
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +54,7 @@ public class Stage3Activity extends AppCompatActivity {
 
        name = intent.getStringExtra("name");
        email = intent.getStringExtra("email");
+       message = "No message included";
 
        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("viewImage");
        binding.viewPhotoS3.setImageBitmap(bitmap);
@@ -50,5 +72,47 @@ public class Stage3Activity extends AppCompatActivity {
 
        binding.textName.setText(name);
        binding.textEmail.setText(email);
+       binding.edtMessage.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+           }
+
+           @Override
+           public void afterTextChanged(Editable editable) {
+               message = binding.edtMessage.getText().toString();
+           }
+       });
+
+       binding.btnSendMail.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               composeEmail(email, "Hi, This is an email",uri, message);
+           }
+       });
+    }
+
+    @SuppressLint({"IntentReset", "QueryPermissionsNeeded"})
+    private void composeEmail(String address, String subject, Uri attachment, String message){
+       Intent sendMailIntent = new Intent(Intent.ACTION_SEND);
+       sendMailIntent.setType("*/*");
+       sendMailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{address});
+       sendMailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+       sendMailIntent.putExtra(Intent.EXTRA_STREAM, attachment);
+       sendMailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        resultMailLauncher.launch(sendMailIntent);
+
+       /*if (sendMailIntent.resolveActivity(getPackageManager()) != null) {
+           binding.btnSendMail.setText("Email sent");
+           startActivity(sendMailIntent);
+
+
+       }*/
     }
 }
